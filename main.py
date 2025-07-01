3ce188f63dbd36272712a18c1d618085426e9c4c
ninjamar <70116773+ninjamar@users.noreply.github.com>
2025-07-01 14:44:49 -0500

    Add stretch goal implementation

--- main.py
+++ main.py
@@ -3,4 +3,7 @@
 print("Welcome to AI Applications")
 
 name = input("Please enter your name: ")
-print("Hello", name, "Nice to meet you")
+print(f"Hello, {name}. Nice to meet you")
+
+fav = input("What is your favorite programming language? ")
+print(f"Nice choice, {name}. {fav} is a great language!")
